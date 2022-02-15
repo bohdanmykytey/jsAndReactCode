b9cddf97ea07cb97cc298dddf03be329fc1c000d
Bohdan Mykytey <bohdanmykytey@Bohdans-MacBook-Pro.local>
2022-02-15 17:15:13 -0500

    updating repo with 4 expenses displaying in the gui

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,17 +1,17 @@
 import React from "react";
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-    const expenseDate = new Date().toDateString()
-    const expenseTitle = 'Car Insurance'
-    const expenseAmount = '$237.45'
+  const expenseDate = new Date().toDateString();
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 235.46;
 
   return (
     <div className="expense-item">
-      <div>{expenseDate} </div>
+      <div>{props.title}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">{expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
       </div>
     </div>
   );
